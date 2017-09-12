9f2e02eaa7efbc85d72e2b84363ddafa0cb38e84
Derek Kramer <derek.kramer88@gmail.com>
2017-09-11 22:33:22 -0600

    🌅 Added sample avatar picture

--- src/index.js
+++ src/index.js
@@ -6,16 +6,20 @@ import registerServiceWorker from './registerServiceWorker';
 
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
-import About from './components/About';
+import Home from './components/Home';
 
 ReactDOM.render(
     <Router>
     <div>
         <Route path='/' component={App}/>
-        <Route exact path='/' component={About}/>
+        <Route exact path='/' component={Home}/>
         {/* <Route path='/projects' component={Projects}/>
         <Route path='/contact' component={Contact}/> */}
     </div>
 </Router>, document.getElementById('root'));
+
+if(module.hot){
+    module.hot.accept();
+}
 
 registerServiceWorker();
