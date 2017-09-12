4c00b9036d93ee5358219db9ab14b4f7c7a58cab
Derek Kramer <derek.kramer88@gmail.com>
2017-09-11 21:54:08 -0600

    🌎 Completed landing page

--- src/index.js
+++ src/index.js
@@ -4,5 +4,16 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+
+ReactDOM.render(
+    <Router>
+    <div>
+        <Route path='/' component={App}/>
+        {/* <Route exact path='/' component={About}/>
+        <Route path='/projects' component={Projects}/>
+        <Route path='/contact' component={Contact}/> */}
+    </div>
+</Router>, document.getElementById('root'));
+
 registerServiceWorker();
