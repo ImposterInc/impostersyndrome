783b06efc1069fb617c3af3e3fc1cdb4f4f017b0
Derek Kramer <derek.kramer88@gmail.com>
2017-09-18 23:31:23 -0600

    👾 Added state change and started onSubmit for login in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,44 @@ import React, {Component} from 'react';
 import '../App.css';
 
 export default class Header extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            userEntry: '',
+            passEntry: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    handleSubmit(event){
+        const { userEntry, passEntry } = this.state;
+
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className='page-header'>
                 <header>
                     <h1>Imposter Syndrome</h1>
-                    <nav>
-                        <form>
-                            <input type="text" placeholder="Username"></input>
-                            <input type="text" placeholder="Password"></input>
-                            <button type="submit">Log In</button>
-                        </form>
-                    </nav>
+                    <Login handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
                 </header>
             </div>
         );
     }
 }
+
+const Login = ({ handleChange, handleSubmit }) => <nav>
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="userEntry" placeholder="Username" onChange={handleChange}></input>
+            <input type="password" name="passEntry" placeholder="Password" onChange={handleChange}></input>
+            <button type="submit">Log In</button>
+        </form>
+    </nav>
